8e04fcf4c2166b46e4357d9befafec2e5a5ef1a3
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-05-30 16:47:16 -0500

    Create 1.4.-Test.js

---
+++ 1.4.-Test.js
@@ -0,0 +1,53 @@
+var contactNewsletterForm = 
+{
+	ready : function()
+	{
+		$('input#contactNewsletterFormSubmit').click(
+			function(event)
+			{
+				var input = $(this);
+				input.attr("disabled", true);
+
+				if(!contactNewsletterForm.validate())
+				{
+					alert("Proporcina nombre y apellido");
+					input.removeAttr('disabled');
+					event.preventDefault();
+				}
+				else
+				{
+					$("form#contactNewsletterForm").submit();
+				}
+			});
+	},
+
+	validate : function()
+	{
+		var hasRequiredValues = true;
+
+		$("form#contactNewsletterForm").find('input, select, textarea').each(
+			function()
+			{
+				var node = $(this);
+
+				if(node.is('[required]'))
+				{
+					var value = node.val();
+
+					if(!value)
+					{
+						hasRequiredValues = false;
+						return false;
+					}
+				}
+			});
+
+		return hasRequiredValues;
+	}
+};
+
+$(document).ready(
+	function()
+	{
+		contactNewsletterForm.ready();
+	});
